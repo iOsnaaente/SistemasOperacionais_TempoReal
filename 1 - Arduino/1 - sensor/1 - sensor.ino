9068959ce67cc1a9ad7bdbc67096869f6537391a
iOsnaaente <bruno.bielsam.1205@hotmail.com>
2021-03-04 16:06:56 -0300

    update2 - 040321 - bruno

--- 1 - Arduino/1 - sensor/1 - sensor.ino
+++ 1 - Arduino/1 - sensor/1 - sensor.ino
@@ -1,16 +1,34 @@
-#define BUFFER_LEN 32
+#define INT32LEN sizeof(int32_t)
 
-float read_sensor();
+int32_t read_sensor();
+
+long int time_ms = 500; // milisegundos 
+long int time_c;  
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.write( read_sensor() );
-  delay(500);
+
+  time_c = millis();
+
+  union {
+    int32_t val_sensor = read_sensor();
+    byte bytes_val[INT32LEN]; 
+  } var;
+
+  for ( int i = 0; i < INT32LEN; i++ ){
+    Serial.print(var.bytes_val[i], HEX);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // Garante periodicidade 
+  delay( time_ms - ( millis() - time_c ) );
+
 }
 
-float read_sensor(){
-  return random(0,100);
+int32_t read_sensor(){
+  return (int32_t)random(0, 0xfffffff );
 }
