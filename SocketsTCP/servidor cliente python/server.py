a4bb3ca1680274d36da299c2ff4db422ea7b36aa
Vitor <vitorfernandesbastos@gmail.com>
2021-03-13 02:29:59 -0300

    att 13/03

--- SocketsTCP/servidor cliente python/server.py
+++ SocketsTCP/servidor cliente python/server.py
@@ -19,7 +19,7 @@ class Thatuador(Thread):
         while LIST:
             for msg in LIST:
                 #print(LIST)
-                self.info.request.send(msg.encode()) # dando merda nesse self deve ser passado o socket
+                self.info.request.send(msg.encode())
                 #info.remove(msg)
                 LIST.remove(msg)
         #print("nao entrou")
@@ -41,30 +41,20 @@ def agora():
 def situacao(self,strinfodata):
     if strinfodata[0:1] == "A":
         atuador = self
-        #print("atuador")
-        #print("type",type(self))
-        #print(atuador)
         th = Thatuador(atuador)
         th.start()
-        #self.request.send(strinfodata.encode())   #aqui deu bom +-
     elif strinfodata[0:1] == "S":
         th = Thsensor(strinfodata)
         th.start()
-        #print("sensor")
 
 class LidarcomCliente( socketserver.BaseRequestHandler ):                                                # Classe para lidar com a conexao dos clientes
 
     def handle(self):
-        #print(self.client_address, agora() )
-        #print(self)
         while True:                                                                                       # Laço de execução do Servidor
             infodata = self.request.recv(1024)                                                            # informação que recebeu do cliente
             strinfodata = infodata.decode("utf-8")
-            #print( self.client_address, strinfodata )                                                     # Qual cliente enviou a mensagem e a mensagem
             if not infodata:
                 break
-            #resposta = 'msg: %s, DataHora %s' % (strinfodata, agora())          # cria uma resposta p/ enviar
-            #self.request.send(resposta.encode())                                                          # envia p/ cliente uma resposta qualquer
             situacao(self,strinfodata)
 
         self.request.close()
